adb26aab5293433d632a92b5b3dd024bb529d668
cbrun <cedric.brun@obeo.fr>
2015-08-27 17:59:46 +0200

    [478080] Trigger SaveSessionWhenNoDialectEditorsListener in postcommit
    Transform the SaveSessionWhenNoDialectEditorsListener into a post-commit
    listener, in doing so :
    - it gets triggered when the Session has finished processing all its
    events from the workspace
    - it doesn't have to analyze individual resource change events to detect
    if the project is being renamed/moved/deleted
    - it can directly ask for the session status (SYNC or DIRTY) and decide
    upon that.
    
    Bug: 478080
    Change-Id: I637625f372aa1ea29588ecca9ae52f183c2b9882
    Signed-off-by: Cedric Brun <cedric.brun@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/internal/session/SaveSessionWhenNoDialectEditorsListener.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/internal/session/SaveSessionWhenNoDialectEditorsListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011, 2016 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2011, 2017 THALES GLOBAL SERVICES and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,40 +10,29 @@
  *******************************************************************************/
 package org.eclipse.sirius.ui.business.internal.session;
 
-import java.util.Collection;
-
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.workspace.util.WorkspaceSynchronizer;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListenerImpl;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.sirius.business.api.session.Session;
 import org.eclipse.sirius.business.api.session.SessionStatus;
 import org.eclipse.sirius.business.internal.session.danalysis.SaveSessionJob;
-import org.eclipse.sirius.common.tools.api.resource.ResourceSetSync;
-import org.eclipse.sirius.common.tools.api.resource.ResourceSetSync.ResourceStatus;
-import org.eclipse.sirius.common.tools.api.resource.ResourceSyncClient;
-import org.eclipse.sirius.ext.base.Option;
 import org.eclipse.sirius.ui.business.api.preferences.SiriusUIPreferencesKeys;
 import org.eclipse.sirius.ui.business.api.session.EditingSessionEvent;
 import org.eclipse.sirius.ui.business.api.session.IEditingSession;
 import org.eclipse.sirius.ui.business.api.session.SessionUIManager;
 import org.eclipse.sirius.viewpoint.provider.SiriusEditPlugin;
 
-import com.google.common.collect.Sets;
-
 /**
- * A listener to resource set change which save session if there is no dialect
- * editor opens.
+ * A listener to resource set change which save session if there is no dialect editor opens.
  * 
  * @author mchauvin
  */
-public class SaveSessionWhenNoDialectEditorsListener implements ResourceSyncClient {
+public class SaveSessionWhenNoDialectEditorsListener extends ResourceSetListenerImpl {
 
     /**
-     * The session from which Sirius resources modifications are listened to
-     * know if a save must be execute when no editors are opened.
+     * The session from which Sirius resources modifications are listened to know if a save must be execute when no
+     * editors are opened.
      */
     protected final Session session;
 
@@ -65,20 +54,34 @@ public class SaveSessionWhenNoDialectEditorsListener implements ResourceSyncClie
      * Register this listener.
      */
     public void register() {
-        ResourceSetSync.getOrInstallResourceSetSync(session.getTransactionalEditingDomain()).registerClient(this);
+        session.getTransactionalEditingDomain().addResourceSetListener(this);
     }
 
     /**
      * Unregister this listener.
      */
     public void unregister() {
-        // Do not call ResourceSetSync.getOrInstallResourceSetSync as the
-        // ResourceSetSync could already have been removed.
-        Option<ResourceSetSync> resourceSetSync = ResourceSetSync.getResourceSetSync(session.getTransactionalEditingDomain());
-        if (resourceSetSync.some()) {
-            resourceSetSync.get().unregisterClient(this);
-        }
+        session.getTransactionalEditingDomain().removeResourceSetListener(this);
         saveSessionJob = null;
+    }
+
+    @Override
+    public boolean isPostcommitOnly() {
+        return true;
+    }
+
+    @Override
+    public void resourceSetChanged(ResourceSetChangeEvent event) {
+        super.resourceSetChanged(event);
+        if (activation && newMode()) {
+            final IEditingSession editingSession = SessionUIManager.INSTANCE.getUISession(session);
+            if (session.isOpen() && editingSession != null && editingSession.getEditors().isEmpty() && SessionStatus.DIRTY.equals(session.getStatus())) {
+                if (saveSessionJob == null || saveSessionJob.getState() == Job.NONE) {
+                    saveSessionJob = new SaveSessionJob(session);
+                    saveSessionJob.schedule();
+                }
+            }
+        }
     }
 
     /**
@@ -103,93 +106,6 @@ public class SaveSessionWhenNoDialectEditorsListener implements ResourceSyncClie
     private boolean newMode() {
         final IPreferenceStore preferenceStore = SiriusEditPlugin.getPlugin().getPreferenceStore();
         return preferenceStore != null && preferenceStore.getBoolean(SiriusUIPreferencesKeys.PREF_SAVE_WHEN_NO_EDITOR.name());
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.resource.ResourceSyncClient#statusChanged(org.eclipse.emf.ecore.resource.Resource,
-     *      org.eclipse.sirius.common.tools.api.resource.ResourceSetSync.ResourceStatus,
-     *      org.eclipse.sirius.common.tools.api.resource.ResourceSetSync.ResourceStatus)
-     */
-    @Override
-    public void statusChanged(Resource resource, ResourceStatus oldStatus, ResourceStatus newStatus) {
-        // Do nothing while processing,
-        // see statusesChanged(Collection<ReResourceStatusChange>)
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void statusesChanged(Collection<ResourceStatusChange> changes) {
-        if (activation && newMode()) {
-            statusChangedInternal(changes);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.eclipse.sirius.common.tools.api.resource.ResourceSyncClient#statusChanged(org.eclipse.emf.ecore.resource.Resource,
-     *      org.eclipse.sirius.common.tools.api.resource.ResourceSetSync.ResourceStatus,
-     *      org.eclipse.sirius.common.tools.api.resource.ResourceSetSync.ResourceStatus)
-     */
-    private void statusChangedInternal(Collection<ResourceStatusChange> changes) {
-        final IEditingSession editingSession = SessionUIManager.INSTANCE.getUISession(session);
-
-        if (session.isOpen() && editingSession != null && editingSession.getEditors().isEmpty() && resourceNoMoreInSync(changes)) {
-            if (wasProjectDeletedOrRenamed(changes))
-                return;
-
-            if (SessionStatus.DIRTY.equals(session.getStatus())) {
-                if (saveSessionJob == null || saveSessionJob.getState() == Job.NONE) {
-                    preSave();
-                    saveSessionJob = new SaveSessionJob(session);
-                    saveSessionJob.schedule();
-                }
-            }
-        }
-    }
-
-    /**
-     * This method allows to do some treatments before saving.
-     * 
-     * WARNING : Be careful not to break default Sirius saving behavior when
-     * overriding this method.
-     * 
-     */
-    protected void preSave() {
-        // Do nothing
-    }
-
-    private boolean wasProjectDeletedOrRenamed(Collection<ResourceStatusChange> changes) {
-        Collection<IProject> projects = Sets.newHashSet();
-        for (ResourceStatusChange change : changes) {
-            IFile file = WorkspaceSynchronizer.getFile(change.getResource());
-            if (file != null) {
-                IProject project = file.getProject();
-                if (project != null) {
-                    projects.add(project);
-                }
-            }
-        }
-
-        for (IProject project : projects) {
-            if (!project.exists()) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean resourceNoMoreInSync(Collection<ResourceStatusChange> changes) {
-        boolean resourcesNoMoreInSync = false;
-        for (ResourceStatusChange change : changes) {
-            ResourceStatus newStatus = change.getNewStatus();
-            resourcesNoMoreInSync = resourcesNoMoreInSync || !newStatus.equals(ResourceStatus.SYNC);
-        }
-        return resourcesNoMoreInSync;
     }
 
 }

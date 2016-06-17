58ced8fb239eba3d912ef328f99f39a775091ac9
Laurent Fasani <laurent.fasani@obeo.fr>
2016-06-17 11:51:50 +0200

    [494766] Fix repair and migration participant
    * DView.eAllContent won't get the content of DRepresentation as
    DRepresentation is not owned by DView anymore.
    DViewQuery.getAllContentInRepresentations(DView, Predicate<? super
    EObject>) is added to manage this need.
    
    Bug: 494766
    Change-Id: Ib5389972d1491b690d73b27ca88ebd94086e7e0c
    Signed-off-by: Laurent Fasani <laurent.fasani@obeo.fr>

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/api/query/DViewQuery.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/api/query/DViewQuery.java
@@ -10,13 +10,19 @@
  *******************************************************************************/
 package org.eclipse.sirius.business.api.query;
 
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sirius.viewpoint.DRepresentation;
 import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
 import org.eclipse.sirius.viewpoint.DView;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
+import com.google.common.collect.UnmodifiableIterator;
 
 /**
  * A class aggregating all the queries (read-only!) having a {@link DView} as a
@@ -60,6 +66,25 @@ public final class DViewQuery {
             }
         }
         return representations;
+    }
+
+    /**
+     * Get all the EObject following a predicate from all the
+     * {@link DRepresentation} inside the {@link DView} ({@link DRepresentation}
+     * s are not owned by the {@link DView}).
+     * 
+     * @param predicate
+     *            predicate used to filter the result
+     * @return the result iterator
+     */
+    public Iterator<EObject> getAllContentInRepresentations(final Predicate<? super EObject> predicate) {
+        Iterator<EObject> iterator = Collections.emptyIterator();
+        List<DRepresentation> allRepresentations = this.getLoadedRepresentations();
+        for (DRepresentation dRepresentation : allRepresentations) {
+            UnmodifiableIterator<EObject> currentIterator = Iterators.filter(dRepresentation.eAllContents(), predicate);
+            iterator = Iterators.concat(iterator, currentIterator);
+        }
+        return iterator;
     }
 
 }

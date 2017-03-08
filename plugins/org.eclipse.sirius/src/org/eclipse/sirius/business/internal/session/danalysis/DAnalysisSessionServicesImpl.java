ff527bf6ded085eb89c0d50649fbdb997f51e057
Florian Barbin <florian.barbin@obeo.fr>
2017-03-08 10:44:34 +0100

    [DRAFT 493353] Search featureExtensionsData into current object resource
    Bug: 493353
    Change-Id: Ibb1075b40eec66ebddc2e1d6b2b79e57642ae9a5
    Signed-off-by: Florian Barbin <florian.barbin@obeo.fr>

--- plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/DAnalysisSessionServicesImpl.java
+++ plugins/org.eclipse.sirius/src/org/eclipse/sirius/business/internal/session/danalysis/DAnalysisSessionServicesImpl.java
@@ -314,7 +314,15 @@ public class DAnalysisSessionServicesImpl implements SessionService, DAnalysisSe
 
     private Collection<EObject> getFeatureExtensionsData(final EObject associatedInstance, Collection<Resource> resources) {
         final Collection<EObject> datas = Lists.newArrayList();
-        for (final Resource res : resources) {
+        Collection<Resource> allResources = Lists.newArrayList();
+        allResources.addAll(resources);
+        if (associatedInstance != null) {
+            Resource associatedInstanceResource = associatedInstance.eResource();
+            if (associatedInstanceResource != null) {
+                allResources.add(associatedInstanceResource);
+            }
+        }
+        for (final Resource res : allResources) {
             for (final EObject object : res.getContents()) {
                 if (object instanceof DFeatureExtension) {
                     datas.add(object);

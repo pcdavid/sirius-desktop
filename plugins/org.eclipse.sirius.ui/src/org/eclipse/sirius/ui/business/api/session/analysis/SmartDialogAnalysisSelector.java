696122626f74addb370d5ab63237b8b34d693e50
Steve Monnier <steve.monnier@obeo.fr>
2020-10-12 16:18:41 +0200

    [567805] Update default Analysis selector dialog
    The analysis selector has been updated to be more user friendly and
    allows to display message in the header.
    
    Bug:567805
    Change-Id: I999ea6d9488c9a800af5b650d8e05627169a64c1
    Signed-off-by: Steve Monnier <steve.monnier@obeo.fr>

--- plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/api/session/analysis/SmartDialogAnalysisSelector.java
+++ plugins/org.eclipse.sirius.ui/src/org/eclipse/sirius/ui/business/api/session/analysis/SmartDialogAnalysisSelector.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008-2015 THALES GLOBAL SERVICES and others.
+ * Copyright (c) 2008-2020 THALES GLOBAL SERVICES and others.
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
@@ -12,26 +12,28 @@
  *******************************************************************************/
 package org.eclipse.sirius.ui.business.api.session.analysis;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.emf.transaction.RunnableWithResult;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.window.Window;
+import org.eclipse.sirius.business.api.query.DRepresentationQuery;
+import org.eclipse.sirius.business.api.query.EObjectQuery;
 import org.eclipse.sirius.business.api.session.danalysis.DAnalysisSelector;
 import org.eclipse.sirius.common.ui.tools.api.util.EclipseUIUtil;
+import org.eclipse.sirius.ui.tools.api.dialogs.AnalysisSelectorFilteredItemsSelectionDialog;
+import org.eclipse.sirius.ui.tools.api.views.ViewHelper;
+import org.eclipse.sirius.ui.tools.internal.views.common.SessionLabelProvider;
 import org.eclipse.sirius.viewpoint.DAnalysis;
 import org.eclipse.sirius.viewpoint.DRepresentation;
-import org.eclipse.sirius.viewpoint.provider.Messages;
-import org.eclipse.sirius.viewpoint.provider.SiriusEditPlugin;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.widgets.Composite;
+import org.eclipse.sirius.viewpoint.DRepresentationDescriptor;
+import org.eclipse.sirius.viewpoint.ViewpointPackage;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.IDecoratorManager;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.eclipse.swt.widgets.Shell;
 
 /**
  * A dialog which select smartly analysis.
@@ -39,15 +41,25 @@ import org.eclipse.ui.dialogs.ElementListSelectionDialog;
  * @author mchauvin
  */
 public class SmartDialogAnalysisSelector implements DAnalysisSelector {
+    
+    /**
+     * The dialog that displays the analysis where the representation will be placed.
+     */
+    protected AnalysisSelectorFilteredItemsSelectionDialog dialog;
 
     @Override
     public DAnalysis selectSmartlyAnalysisForAddedResource(final Resource resource, final Collection<DAnalysis> allAnalysis) {
-        return selectSmartlyAnalysis(allAnalysis);
+        return selectSmartlyAnalysis(allAnalysis, null);
     }
 
     @Override
     public DAnalysis selectSmartlyAnalysisForAddedRepresentation(final DRepresentation representation, final Collection<DAnalysis> allAnalysis) {
-        return selectSmartlyAnalysis(allAnalysis);
+        DAnalysis selectSmartlyAnalysis = selectSmartlyAnalysis(allAnalysis, representation.getName());
+        if (selectSmartlyAnalysis == null) {
+            DRepresentationDescriptor representationDescriptor = new DRepresentationQuery(representation).getRepresentationDescriptor();
+            selectSmartlyAnalysis = (DAnalysis) new EObjectQuery(representationDescriptor).getFirstAncestorOfType(ViewpointPackage.eINSTANCE.getDAnalysis()).get();
+        }
+        return selectSmartlyAnalysis;
     }
 
     /**
@@ -57,49 +69,38 @@ public class SmartDialogAnalysisSelector implements DAnalysisSelector {
      *            all available analysis
      * @return selected analysis
      */
-    private DAnalysis selectSmartlyAnalysis(final Collection<DAnalysis> allAnalysis) {
+    private DAnalysis selectSmartlyAnalysis(final Collection<DAnalysis> allAnalysis, String representationName) {
 
-        final ILabelProvider provider = new AdapterFactoryLabelProvider(SiriusEditPlugin.getPlugin().getItemProvidersAdapterFactory()) {
-            private IDecoratorManager decoratorManager = PlatformUI.getWorkbench().getDecoratorManager();
+        final ILabelProvider labelProvider = getLocationLabelProvider();
 
-            @Override
-            public String getText(final Object object) {
-                if (object instanceof DAnalysis) {
-                    return ((DAnalysis) object).eResource().getURI().toString();
-                }
-                return super.getText(object);
-            }
-
-            @Override
-            public Image getImage(Object object) {
-                return decoratorManager.decorateImage(super.getImage(object), object);
-            }
-        };
         RunnableWithResult<Object> runnable = new RunnableWithResult.Impl<Object>() {
 
             @Override
             public void run() {
-                final ElementListSelectionDialog dialog = new ElementListSelectionDialog(Display.getDefault().getActiveShell(), provider) {
+                
+                dialog = createAnalysisSelectorDialog(Display.getDefault().getActiveShell(), allAnalysis.iterator().next(), allAnalysis, new ArrayList<>(allAnalysis), labelProvider);
+                dialog.setDetailsLabelProvider(labelProvider);
+                dialog.setListLabelProvider(labelProvider);
+                dialog.setSeparatorLabel("Other Fragments"); //$NON-NLS-1$
+                if (representationName != null && !representationName.isEmpty()) {
+                    dialog.setTitle("Location selection for '" + representationName + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+                    dialog.setMessage("Select a location where the representation '" + representationName + "' will be placed."); //$NON-NLS-1$ //$NON-NLS-2$
+                } else {
+                    dialog.setTitle("Location selection"); //$NON-NLS-1$
+                    dialog.setMessage("Select a location"); //$NON-NLS-1$
+                }
 
-                    /**
-                     * do not allow cancel {@inheritDoc}
-                     *
-                     * @see org.eclipse.ui.dialogs.SelectionDialog#createButtonsForButtonBar(org.eclipse.swt.widgets.Composite)
-                     */
-                    @Override
-                    protected void createButtonsForButtonBar(final Composite parent) {
-                        createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
-                    }
+                if (allAnalysis.iterator().next() != null) {
+                    dialog.setInitialElementSelections(Collections.singletonList(allAnalysis.iterator().next()));
+                }
 
-                };
-
-                dialog.setTitle(Messages.SmartDialogAnalysisSelector_title);
-                dialog.setMessage(Messages.SmartDialogAnalysisSelector_message);
-                dialog.setElements(allAnalysis.toArray());
                 if (dialog.open() == Window.OK) {
                     if (dialog.getFirstResult() != null) {
                         setResult(dialog.getFirstResult());
                     }
+                } else {
+                    // Box closed by cancel, ESC key ...
+                    setResult(null);
                 }
             }
         };
@@ -108,6 +109,48 @@ public class SmartDialogAnalysisSelector implements DAnalysisSelector {
         if (runnable.getResult() instanceof DAnalysis) {
             return (DAnalysis) runnable.getResult();
         }
-        return (DAnalysis) allAnalysis.toArray()[0];
+        return null;
     }
+
+    /**
+     * Initializes the dialog that will be used for selecting the targeted {@link DAnalysis}.
+     * 
+     * @param shell
+     *            shell to parent the dialog on
+     * @param bestCandidate
+     *            the best candidate that will be selected by default
+     * @param allAnalysis
+     *            all the analysis available
+     * @param bestCandidates
+     *            list of best candidates
+     * @param labelProvider
+     *            the label provider that will be used to present the candidates
+     * @return the dialog that will be used for selecting the targeted {@link DAnalysis}
+     */
+    protected AnalysisSelectorFilteredItemsSelectionDialog createAnalysisSelectorDialog(Shell shell, DAnalysis bestCandidate, Collection<DAnalysis> allAnalysis, List<DAnalysis> bestCandidates,
+            ILabelProvider labelProvider) {
+        return new AnalysisSelectorFilteredItemsSelectionDialog(Display.getDefault().getActiveShell(), allAnalysis.iterator().next(), allAnalysis, new ArrayList<>(allAnalysis), labelProvider);
+    }
+
+    protected ILabelProvider getLocationLabelProvider() {
+        return new SessionLabelProvider(ViewHelper.INSTANCE.createAdapterFactory()) {
+            @Override
+            public String getText(final Object object) {
+                String result;
+                if (object instanceof DAnalysis) {
+                    DAnalysis dAnalysis = (DAnalysis) object;
+                    if (dAnalysis.eResource().getURI().isPlatformResource()) {
+                        result = "Local (in " + dAnalysis.eResource().getURI().path().replace("/resource/", "/") + ")"; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+                    } else {
+                        result = "in " + dAnalysis.eResource().getURI().path(); //$NON-NLS-1$
+                    }
+                } else {
+                    result = super.getText(object);
+                }
+                return result;
+            }
+
+        };
+    }
+
 }
